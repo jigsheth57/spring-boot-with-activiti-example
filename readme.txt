2c969b69487bf1e92c1660f8ddbc80c34e9808f8
Jignesh Sheth <jsheth@pivotal.io>
2016-09-01 19:45:41 -0500

    update

--- readme.txt
+++ readme.txt
@@ -1,15 +1,18 @@
 # Test the app
 
-curl -u admin:admin -H "Content-Type: application/json" -d '{"name":"John Doe", "email": "john.doe@alfresco.com", "phoneNumber":"123456789"}' https://activitiexample.cfapps.io/startProcess
+curl -u admin:admin -H "Content-Type: application/json" -d '{"name":"John Doe", "email": "john.doe@alfresco.com", "phoneNumber":"123456789"}' http://activitiexample-dev.cfapps.io/startProcess
 
-curl -u admin:admin -H "Content-Type: application/json" -d '{"telephoneInterviewOutcome" : "true"}' https://activitiexample.cfapps.io/completeTelephoneInterview/2501
+curl -u admin:admin -H "Content-Type: application/json" -d '{"telephoneInterviewOutcome" : "true"}' http://activitiexample-dev.cfapps.io/completeTelephoneInterview/2501
 
-curl -u admin:admin -H "Content-Type: application/json" -d '{"techOk" : "true"}' https://activitiexample.cfapps.io/completeTechInterview/2501
+curl -u admin:admin -H "Content-Type: application/json" -d '{"techOk" : "true"}' http://activitiexample-dev.cfapps.io/completeTechInterview/2501
 
-curl -u admin:admin -H "Content-Type: application/json" -d '{"financialOk" : "false"}' https://activitiexample.cfapps.io/completeFinancialNegotiation/2501
+curl -u admin:admin -H "Content-Type: application/json" -d '{"financialOk" : "false"}' http://activitiexample-dev.cfapps.io/completeFinancialNegotiation/2501
 
-curl -u admin:admin https://activitiexample.cfapps.io/tasks/2501
+curl -u admin:admin http://activitiexample-dev.cfapps.io/tasks/2501
 
+/history/historic-activity-instances
+/runtime/process-instances
+/runtime/tasks
 
 # Deploy via Concourse
 cd ci
--- src/main/java/org/activiti/HireProcessApp.java
+++ src/main/java/org/activiti/HireProcessApp.java
@@ -7,53 +7,13 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
-@Controller
 public class HireProcessApp {
 
     public static void main(String[] args) {
         SpringApplication.run(HireProcessApp.class, args);
     }
-
-    @Bean
-    public Docket newsApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-        		.apiInfo(apiInfo())
-        		.select()                                  
-                .apis(RequestHandlerSelectors.any())              
-                .paths(PathSelectors.any())                          
-                .build();  
-    }
-     
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("BPM Hire Process Demo")
-                .description("Hire Process BPM demo using Spring JPA, REST, Activiti with Swagger")
-                .termsOfServiceUrl("http://pivotal.io/")
-                .contact(new Contact("Jignesh Sheth",null,null))
-                .license("Apache License Version 2.0")
-                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
-                .version("2.0")
-                .build();
-    }
-    
-	@RequestMapping("/")
-	public String home() {
-		return "redirect:/swagger-ui.html";
-	}
 
     @Bean
     InitializingBean usersAndGroupsInitializer(final IdentityService identityService) {
